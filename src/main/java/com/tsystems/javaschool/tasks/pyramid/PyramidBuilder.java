6976e7e7946d1de19ee7cfcadd619b7d8916315b
PavelSteel <misskiisteel@gmail.com>
2019-06-20 00:40:06 +0300

    Created the logic of building pyramids

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,6 +1,7 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PyramidBuilder {
 
@@ -13,8 +14,51 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+        boolean mark = false;
+        int[][] arr;
+        int size = inputNumbers.size();
+        int count = 0;
+        int rows = 1;
+        int columns = 1;
+
+        while (count < size) {
+            count += rows;
+            rows++;
+            columns += 2;
+        }
+        rows = rows - 1;
+        columns = columns - 2;
+
+        if (size == count) {
+            mark = true;
+        }
+        try {
+            if (mark) {
+                List<Integer> sorted = inputNumbers.stream().sorted().collect(Collectors.toList());
+
+                arr = new int[rows][columns];
+
+                int center = columns / 2;
+                int sInd = 0;
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j <= i; j++) {
+                        arr[i][center - i + j * 2] = sorted.get(sInd++);
+                    }
+                }
+
+                for (int[] a : arr)
+                {
+                    for (int b : a)
+                        System.out.print(b + "   ");
+                    System.out.println();
+                }
+            }else throw new CannotBuildPyramidException();
+
+        } catch (Exception e) {
+            throw new CannotBuildPyramidException();
+        }
+
+        return arr;
     }
 
 
